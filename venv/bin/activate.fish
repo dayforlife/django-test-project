8dcf1088355a82d8efdb16703b9aeca299bfc5ca
kani0798 <arstanbaevakani@gmail.com>
2020-12-03 19:38:29 +0600

    4 часть админ панель

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/hello/Desktop/locallibrary/venv"
+set -gx VIRTUAL_ENV "/Users/hello/Desktop/django-test-project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
